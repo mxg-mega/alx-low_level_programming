4d6ff3061b4c9ec4933cd982f8cfe9669292bf3f
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2022-09-09 14:06:15 -0700

    fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,8 @@
 
 int main(void)
 {
+	/*the puts function is an stdout like printf , 
+	 * also strings are initialized as arrays because there is no data type string*/
 	char message[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(message);
