b106ca1993780b79c91e2c395a0c2d2e76252e8d
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2022-09-09 01:58:06 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 int main(void)
 {
 	/*this function put print out strings where strings in c are initialized as arrays*/
-	char message[] = "\"Programming is like building a multilingual puzzle\n";
+	char message = "\"Programming is like building a multilingual puzzle";
 
 	puts(message);
 	return (0);
