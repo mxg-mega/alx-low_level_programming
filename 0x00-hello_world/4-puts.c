bf2300c6a61efa8c0ecfe69105bbe09406897e72
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2022-09-09 08:52:05 +0000

    first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
 int main(void)
 {
 	/*this function put print out strings where strings in c are initialized as arrays*/
-	char message[] = "\"Programming is like building a multilingual puzzle\n"; 
-	puts(message); 
-	return (0); 
+	char message[] = "\"Programming is like building a multilingual puzzle\n";
+
+	puts(message);
+	return (0);
 }
