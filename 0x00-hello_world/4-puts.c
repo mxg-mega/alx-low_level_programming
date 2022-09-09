0c06990998b173bfac497d8c97f4962736bff207
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2022-09-09 14:20:19 -0700

    fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-
-int main(void)/*this is the main method*/
+/** the progam prints a string using put method*/
+int main(void)
 {
 	/*puts is like printf,also strings are initialized as arrays*/
 	char message[] = "\"Programming is like building a multilingual puzzle";
