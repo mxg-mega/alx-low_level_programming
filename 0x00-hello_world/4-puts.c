2f9b6af55b87617b33de31736d000dc89bd176aa
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2022-09-09 07:34:26 +0000

    first c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+
+int main()
+{
+	puts("Programming is like building a multilinguial puzzle\n");
+
+	return 0;
+}
+string puts(string message)
+{
+	return message;
+
+}
