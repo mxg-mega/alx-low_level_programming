209a8208bc8e51fcd1843a3d2f4d0f6d02792a73
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2022-09-09 14:38:36 -0700

    all data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Size of char: 1 byte(s) \nSize of an int: 4 byte(s) \nSize of long int: 4 byte(s) \nSize of long long int: 8 byte(s) \nSize of float: 4 byte(s)");
+	return (0);
+}
