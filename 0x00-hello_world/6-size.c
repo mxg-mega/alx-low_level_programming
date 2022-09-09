648327a5dab228395b64acd5677346ba9a4f3de9
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2022-09-09 14:43:25 -0700

    all data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ *prints the data types
+ * */
 int main(void)
 {
-	printf("Size of char: 1 byte(s) \nSize of an int: 4 byte(s) \nSize of long int: 4 byte(s) \nSize of long long int: 8 byte(s) \nSize of float: 4 byte(s)");
+	/*printing data types*/
+	printf("Size of a char: 1 byte(s) \nSize of an int: 4 byte(s) \nSize of a long int: 4 byte(s) \nSize of a long long int: 8 byte(s) \n")
+		printf("Size of a float: 4 byte(s)");
 	return (0);
 }
