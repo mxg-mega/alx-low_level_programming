51e5c50a34dcdc8e75c6fe6d67a1f9bc8b95a4a2
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2022-09-10 13:51:44 -0700

    fix the doc to betty style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,12 +7,17 @@
  */
 int main(void)
 {
-	/*printing data types for 64-bits*/
+	/*printing data types for 32-size*/
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of an int: 4 byte(s)\n");
+	printf("Size of a long int: 4 byte(s)\n");
+	printf("Size of a long long int: 8 byte(s)\n");
+	printf("Size of a float: 4 byte(s)\n");
+	/*printing data types for 64-size*/
 	printf("Size of a char: 1 byte(s)\n");
 	printf("Size of an int: 4 byte(s)\n");
 	printf("Size of a long int: 8 byte(s)\n");
 	printf("Size of a long long int: 8 byte(s)\n");
 	printf("Size of a float: 4 byte(s)\n");
-
 	return (0);
 }
