dfc774f10d1e4ba1abe52de658748f832ae44399
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2022-09-10 13:42:59 -0700

    fix the doc to betty style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
 *main - Entry point
-*return: Always 0
+*Return: Always 0
 *Description
 *	prints the data types of size 64 and 32
  */
 int main(void)
 {
-	/*printing data types for 32-bits*/
+	/*printing data types for 64-bits*/
 	printf("Size of a char: 1 byte(s)\n");
 	printf("Size of an int: 4 byte(s)\n");
 	printf("Size of a long int: 8 byte(s)\n");
