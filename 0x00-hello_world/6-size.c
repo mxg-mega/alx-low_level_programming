e69f8da5f09af559f4a26be6c167edd05bb7282a
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2022-09-10 07:44:58 -0700

    usifixing size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,10 +4,16 @@
  * */
 int main(void)
 {
-	/*printing data types*/
+	/*printing data types for 32-bits*/
 	printf("Size of a char: 1 byte(s)\n");
 	printf("Size of an int: 4 byte(s)\n");
 	printf("Size of a long int: 4 byte(s)\n");
+	printf("Size of a long long int: 8 byte(s)\n");
+	printf("Size of a float: 4 byte(s)\n");
+	/*sizes for 64-bit*/
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of an int: 4 byte(s)\n");
+	printf("Size of a long int: 8 byte(s)\n");
 	printf("Size of a long long int: 8 byte(s)\n");
 	printf("Size of a float: 4 byte(s)\n");
 	return (0);
