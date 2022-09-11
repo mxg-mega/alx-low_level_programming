afd7f070c6c0396506fdbb1e27b0e6bc55983f13
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2022-09-11 20:29:19 +0000

    printing alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: 0
+*/
+int main(void)
+{
+	char newline = '\n';
+	for(int i = 97 ;i <= 122; i++)
+		putchar(i);
+	putchar(newline);
+	return (0);
+}
