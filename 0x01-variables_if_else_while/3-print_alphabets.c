cd90fb7a85dd10322cc222455e8701c1e9fba0e7
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2022-09-11 20:52:12 +0000

    print upper and lower cases of the alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Return: 0
+ */
+int main(void)
+{
+	int i = 65;
+	int L = 97;
+	char newline = '\n';
+	
+	while (L <= 122)
+	{
+		putchar(L);
+		L++;
+	}
+	while (i <= 90)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(newline);
+	return (0);
+
+}
