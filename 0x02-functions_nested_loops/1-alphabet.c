8feec630dfea07ce9d583bf25f14bb28be4d9044
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2022-09-14 20:20:47 +0000

    $1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*print_alphabet - function
+*/
+void print_alphabet(void)
+{
+
+}
+/**
+*main - Entry point
+*Return: 0
+*/
+int main(void)
+{
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+this folder daels with functions and nested loops 
+the solutions to th tasks
+i started the project on wednesday september 14 2022
