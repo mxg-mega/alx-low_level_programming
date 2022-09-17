14331445f56ca6c0ba6a024e8f8f6d122b4cbad1
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2022-09-17 20:34:59 +0000

    print a-z 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  *print_alphabet_x10 - function that print a-z 10 times
+  *
+  *Return; there is no return as it is void
+  */
+void print_alphabet_x10(void)
+{
+	int c = 10, L;
+	while (c > 0)
+	{
+		L = 97;
+
+		while (L <= 122)
+		{
+			_putchar(a);
+			L++'
+		}
+		_putchar('\n');
+		c--;
+	}
+}
