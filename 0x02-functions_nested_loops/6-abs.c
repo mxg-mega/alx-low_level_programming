27ceb6894a1efba5ee3802a76bb5dbf2af6218c5
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2022-09-17 22:47:14 +0000

    absolute value of this number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  *_abs - function that determines the absolute value of argument
+  *@int: integer to be determined
+  *
+  *Return: the absolute value of the argument
+  */
+int _abs(int)
+{
+	int n;
+
+	if (n < 0)
+	{
+		n = n * -1;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
