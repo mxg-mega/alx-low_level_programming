2583b929cc6daaf6ca9bd5c2617ac9bbf4bfdcca
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2022-09-17 18:53:37 +0000

    a-z

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,18 +2,23 @@
 #include "main.h"
 /**
 *print_alphabet - function
+*Return: no return
 */
-/*void print_alphabet(void)
+void print_alphabet(void)
 {
+	for(int a = 97; a <= 122; a++)
+	{
+		_putchar(a);
+	}
 
-}*/
+}
 /**
 *main - Entry point
 *Return: 0
 */
 int main(void)
 {
-	_putchar('d');
+	print_alphabet();
 	_putchar('\n');
 	return (0);
 }
