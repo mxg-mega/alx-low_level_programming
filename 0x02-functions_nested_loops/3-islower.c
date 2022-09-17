9db3b599ecc2ca31c52fab5828bf74ed60b33386
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2022-09-17 21:16:26 +0000

    is it a lowercase letter

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
-#include <ctype.h>
 /**
   *_islower - function that checks if the argument is lower case
+  *@c: is the int argument that is checked as an ascii if it is under the lower alphabet notaions
   *
   *Return:1 if true and 0 if false
   */
