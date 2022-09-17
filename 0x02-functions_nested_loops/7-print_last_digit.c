dbb31d2a4e9a10c36adb08ed01b4916d22d9c5e1
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2022-09-17 23:00:33 +0000

    last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  *print_last_digit - function determines last digit of a number
+  *@n: last digit
+  *
+  *Return:l value of the last digit of n
+  */
+int print_last_digit(int n)
+{
+	int l;
+
+	l = n % 10;
+	return (l);
+}
