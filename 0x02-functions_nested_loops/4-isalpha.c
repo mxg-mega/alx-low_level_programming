70a74cec1bd6b7ecd88f5d8d0aa08feac2bf9d90
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2022-09-17 22:20:33 +0000

    is this an alphabet ?

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main"
+/**
+  *_isalpha - function to check for alphabetic characters
+  *@c: argument to check if it is an alphabet
+  *
+  *Return: 1 if true 0 if false
+  */
+int _isalpha(int c)
+{
+	if ((c >= 65) && (c <= 90))
+	{
+		return (1);
+	}
+	else if ((c >= 97) && (c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
