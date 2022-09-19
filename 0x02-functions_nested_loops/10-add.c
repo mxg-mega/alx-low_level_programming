41e6ffa491a49990a0f799aae48252b98c7e53bd
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2022-09-19 14:19:45 +0000

    addition of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *add - functions add two integers
+ *@x: first number
+ *@y: second number
+ *
+ *Return: result
+ */
+void add(int x, int y)
+{
+	int sum = x + y;
+
+	return (sum);
+}
