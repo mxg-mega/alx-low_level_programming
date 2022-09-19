960d34b6a262512a45366ae816ad4095560df55c
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2022-09-19 14:08:32 +0000

    no 9 times table fixes

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,13 +12,27 @@ void times_table(void)
 	i = 0;
 	while (i <= 9)
 	{
-		j = 0;
+		_putchar('0');
+		j = 1;
 		while (j <= 9)
 		{
 			int multiplication = i * j;
-			_putchar('0' + multiplication);
+
 			_putchar(',');
 			_putchar(' ');
+			/*this if block takes the product and split into two
+			 *print the first part(tens) by dividing by 10
+			 *after the if block using modulus u print the second digit
+			 */
+			if (multiplication <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((multiplication / 10) + '0');
+			}
+			_putchar((multiplication % 10) + '0');
 			j++;
 		}
 		_putchar('\n');
