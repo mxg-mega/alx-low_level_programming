85d9ba2209f3006db50ec0416522b6d5a2832fe0
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2022-09-25 10:44:19 +0100

    finishing

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*more_numbera - prints numbers to 14
+*
+*Return: returns nothing
+*/
+void more_numbers(void)
+{
+	int n1, n2;
+
+	for (n2 = 0; n2 <= 14; n2++)
+	{
+		for (n2 = 0; n2 <= 14; n2++)
+		{
+			if (n2 > 9)
+			{
+				_putchar((n2 / 10) + '0');
+			}
+			_putchat((n2 % 10) + '0');
+		}
+		_putchar(10);
+	}	
+}
