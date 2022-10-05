3489ec03d50512bb91dc4d77f679164217944a0e
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2022-10-05 13:00:34 +0000

    continue fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Return: always 0
+*/
+int main(void)
+{
+	int n, i, j;
+
+	n = 100;
+	i = 0;
+	while (i <= n)
+	{
+		if ()
+		{
+			putchar((i / 10) + '0');
+		}
+		putchar((i % 10) + '0');
+		putchar(' ');
+		i++;
+	}
+	return (0);
+}
