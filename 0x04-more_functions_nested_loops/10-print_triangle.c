8c7b51f02512bca0baa7a63dda88de44e50387dc
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2022-10-07 11:39:49 +0100

    fix

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_triangle - function prints a triangle
+ *@size: size of the triangle
+ *
+ *Return: no return value
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size > 1)
+	{
+		i = 1;
+		while (i < size)
+		{
+			k = size;
+			while (k > i)
+			{
+				putchar(' ');
+				k--;
+			}
+			j = 0;
+			while (j < i)
+			{
+				putchar('#');
+				j++;
+			}
+			putchar('\n');
+			i++;
+	}
+	}
+	else if (size == 1)
+	{
+		putchar('#');
+	}
+}
+int main(void)
+{
+	print_triangle(1);
+	return 0;
+}
