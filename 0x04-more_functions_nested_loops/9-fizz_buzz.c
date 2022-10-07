4c75442361f22cb6218cdb2c2eb2a46b9941148a
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2022-10-07 11:55:17 +0100

    fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,20 +13,24 @@ int main(void)
 	{
 		if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
+			if (i == 100)
+                {
+                        putchar('\n');
+                }
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
-		if (i == 100)
+		/*if (i == 100)
 		{
 			putchar('\n');
-		}
+		}*/
 		i++;
 	}
 	return (0);
