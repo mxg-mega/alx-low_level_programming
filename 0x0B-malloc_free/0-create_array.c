34b09786cc1b0d3ebbc036e4d75a5a453f4a4d76
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-03-12 17:18:57 +0000

    debugging

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	char *str;
 
 	str = malloc(sizeof(char) * size);
-	if (size == 0 || str == NULL || isalpha(c) == 1)
+	if (size == 0 || str == NULL || isdigit(c) = true)
 	{
 		return (NULL);
 	}
