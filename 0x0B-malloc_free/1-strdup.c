ff4954cf928f0407cd2d63c2fdc1173a95888eb3
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-03-12 17:34:48 +0000

    debugging

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*_strdup - this function creates a memory location pointing to its argument
+*@str: the string to be pointed to
+*Return: a pointer to a character
+*/
+char *_strdup(char *str)
+{
+	char *dup;
+
+	dup = malloc(str);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		dup[0] = str[0];
+		return (dup);
+	}
+
+}
