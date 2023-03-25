caeb9ca1b30eab341458015afc51e6cb91d2fb04
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-03-25 11:00:20 +0000

    fixing_alloc_grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -32,8 +32,9 @@ int **alloc_grid(int width, int height)
 			{
 				/*allocation of memory before assignment*/
 				td_array[j] = malloc(sizeof(int) * width);
-				if (td_array == NULL)
+				if (td_array[j] == NULL)
 				{
+					free(td_array[j]);
 					return (NULL);
 				}
 				else
