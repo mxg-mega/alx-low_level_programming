e505573b83963bd16522081c63a7304f88010fdf
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-04-07 05:10:38 +0000

    free_alloc-fix_alloc_grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,12 +11,13 @@
 int **alloc_grid(int width, int height)
 {
 	int **mee;
-	int x;
+	int x, y;
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
+	mee = malloc(sizeof(int *) * height);
 	if (mee == NULL)
 	{
 		return (NULL);
@@ -34,4 +35,10 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 	}
+	for (x = 0; x < height; x++)
+	{
+		for (y = 0; y < width; y++)
+			mee[x][y] = 0;
+	}
+	return (mee);
 }
