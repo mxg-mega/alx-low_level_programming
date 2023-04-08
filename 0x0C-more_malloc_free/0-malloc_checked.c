21afc982afc23d79fa1f41127d901fdb230ebed1
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-04-08 20:56:25 +0000

    fixed_check_malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,17 +5,14 @@
 void *malloc_checked(unsigned int b)
 {
 	void *alloc;
-	void **p;
 
 	alloc = malloc(b);
 	if (alloc == NULL)
 	{
-		alloc[0] = 98;
 		exit(98);
 	}
 	else
 	{
-		p = &alloc
 		return (p);
 	}
 }
