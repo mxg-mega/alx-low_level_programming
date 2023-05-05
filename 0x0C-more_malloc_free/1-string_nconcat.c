541b025fa01a64b4bd74f675b00e2ea745d3b7d5
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-05-05 11:11:36 +0000

    stringconcat_fixing

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,18 +12,18 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int i, j, len1, len2;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	len1 = 0;
 	while (s1 && s1[len1])
 	{
 		len1++;
 	}
-	len2 = 0;
+
 	while (s2 && s2[len2])
 	{
 		len2++;
 	}
+
 	if (n < len2)
 	{
 		s = malloc(sizeof(char) * (len1 + n + 1));
@@ -32,25 +32,28 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s = malloc(sizeof(char) * (len1 + len2 + 1));
 	}
+
 	if (s == NULL)
 	{
 		return (NULL);
 	}
-	i = 0;
+
 	while (i < len1)
 	{
 		s[i] = s1[i];
 		i++;
 	}
+
 	while (n < len2 && i < (len1 + n))
 	{
-		s[i + 1] = s2[j + 1];
+		s[i++] = s2[j++];
 	}
-	j = 0;
+
 	while (n >= len2 && i < (len1 + len2))
 	{
-		s[i + 1] = s2[j + 1];
+		s[i++] = s2[j++];
 	}
+
 	s[i] = '\0';
 	return (s);
 }
