c2a497367f5834190b8525f00d524471c1198d13
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-05-05 10:54:25 +0000

    stringconcat_fixing

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,12 +12,15 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	unsigned int i, j, len1, len2;
 
+	len1 = 0;
 	while (s1 && s1[len1])
 	{
 		len1++;
 	}
+
+	len2 = 0;
 	while (s2 && s2[len2])
 	{
 		len2++;
@@ -34,6 +37,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
+
+	i = 0;
 	while (i < len1)
 	{
 		s[i] = s1[i];
@@ -43,6 +48,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s[i++] = s2[j++];
 	}
+
+	j = 0;
 	while (n >= len2 && i < (len1 + len2))
 	{
 		s[i++] = s2[j++];
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+_calloc - 
+
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+}
