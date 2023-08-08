c5aaf6303ced0fc1b5448994f51d543fe6d7bc8c
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-08-08 13:17:34 +0000

    created calloc code

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,4 +9,20 @@
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	unsigned int i;
+
+	void *array = malloc(size * nmemb);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < nmemb; i++)
+	{
+		array[i] = 0;
+	}
+	return (array);
 }
