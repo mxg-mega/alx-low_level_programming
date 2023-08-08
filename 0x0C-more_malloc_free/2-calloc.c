a1ea6ac762a61775293dc2cc10c46947422acfec
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-08-08 12:50:25 +0000

    more malloc free update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,8 +2,10 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-_calloc - 
-
+*_calloc - this function allocates memory for an array using malloc
+*@nmemb: elements for allocation
+*@size: size of memory to allocate
+*Return - no return value
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
