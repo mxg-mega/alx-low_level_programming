f9eec96fef5697903109d368bc482a9307018726
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-08-09 20:58:49 +0000

    starting the structure and typedef project and adding dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+this folder contain the solutions under structues in C and type definitions
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *dog - this is a structure that defines three types of variables
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
