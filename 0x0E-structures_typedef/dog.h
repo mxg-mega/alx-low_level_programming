a8eed5501ef8cdb6a58aa503fc2ffcd54c4fbbda
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-08-10 10:57:51 -0700

    finally got 100 for dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * struct dog - a struct data type
+ * @name: name of the doog
+ * @age: age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: this struct contains info about dogs
+ */
 struct dog
 {
 	char *name;
