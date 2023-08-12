ec5af417518299017d137a85334c53def39c7e15
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-08-12 04:00:21 -0700

    defining an alias for struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,12 @@
  *
  * Description: this struct contains info about dogs
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif /*_DOG_H_*/
