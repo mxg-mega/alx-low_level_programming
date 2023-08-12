39302c99ce6aa732f07af2445507ffad7acc4f27
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-08-12 03:35:21 -0700

    trials for print dog file

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - is a funtion that prints struct dog
+ * @d: the structure dog object
+ *
+ * Return: nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		printf("");
+	}
+	if (d->name == NULL)
+	{
+		d->name = "(nil)";
+	}
+	if (d->owner == NULL)
+	{
+		d->owner = "(nil)";
+	}
+	printf("name: %s\n age: %f\n owner: %s\n", d->name, d->age, d->owner);
+}
+
