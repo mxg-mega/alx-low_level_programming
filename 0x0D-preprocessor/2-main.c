4478be9c22977fa1d2ef21235067df2579b525b8
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-09-03 10:05:37 +0000

    fixing main 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - prints the name of the file
  *
- * Return: nothing
+ * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
