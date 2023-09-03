6ebe64fc629dc3096f02fa992bdb1eb0e7046acf
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-09-03 21:08:01 +0000

    fixing printname.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+#include "function_pointers.h"
 
 /**
-  * print_name - contains function pointers to print a string
-  * @name: string to add
-  * @f: pointer to function
-  *
-  * Return: Nothing
-  */
+ * print_name - print name using pointer to function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing
+ **/
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
-		return;	
+		return;
+
 	f(name);
 }
 
