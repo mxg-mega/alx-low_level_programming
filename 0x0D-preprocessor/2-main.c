230982cc842dce63392d161006c7e7c1d2c7aa70
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-09-03 09:54:34 +0000

    fixed to holberton style

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
-  * main - entry point this program displays the file directory
-  *
-  *Return: nothing
-*/
+ * main - entry point this program displays the file directory
+ *
+ *Return: nothing
+ */
 int main()
 {
 	printf("%s\n", __FILE__);
 	return (0);
 }
+
