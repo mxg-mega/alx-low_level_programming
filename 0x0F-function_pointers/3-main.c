6cbb3748a1db347c2f71017dffecaefec623f8ce
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-09-07 22:10:47 +0000

    fixes

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,34 +9,35 @@
   *
   * Return: Always 0
   */
-int main(int argc, char **argv)
+int main(int __attribute__((__unused__)) argc, char **argv)
 {
-	int a, b;
+	int num1, num2;
 	int result;
 	char *operator;
 
 	if (argc < 3)
 	{
-		printf("Error1\n");
+		printf("Error\n");
 		exit (98);
 	}
-	if (get_op_func(op) == NULL || op[1] != '\0')
+	
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
+
+	if (get_op_func(operator) == NULL || operator[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*op == '/' && num2 == 0) ||
-	    (*op == '%' && num2 == 0))
+	if ((*operator == '/' && num2 == 0) || (*operator == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	operator = argv[2];
-	result = (*get_op_func)(operator)(a, b);
+
+	result = get_op_func(operator)(num1, num2);
 
 	printf("%d\n", result);
 	return (0);
