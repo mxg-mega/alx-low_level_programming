d860804ed5b34d36194182665ae303dc8905ef61
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-09-07 21:11:13 +0000

    fixed the calculator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,15 +20,24 @@ int main(int argc, char **argv)
 		printf("Error1\n");
 		exit (98);
 	}
-	if (argv[2] == NULL)
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
-		printf("Error2\n");
-		exit (99);
+		printf("Error\n");
+		exit(99);
 	}
+
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	operator = argv[2];
 	result = (*get_op_func)(operator)(a, b);
+
 	printf("%d\n", result);
 	return (0);
 }
