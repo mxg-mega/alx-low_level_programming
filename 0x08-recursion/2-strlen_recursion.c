1be78a0b5bc59f0135c5700b5f5714b0adcd76af
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-09-30 02:53:59 -0700

    fix strlen recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * _strlen_recursion - function that returns the lenght of string
+  * @s: string
+  * Return: lenght of string
+*/
+int _strlen_recursion(char *s) {
+	if (*s == '\0')
+	{
+		return 0;
+	}
+	else
+	{
+		return 1 + _strlen_recursion(s + 1);
+	}
+}
+
