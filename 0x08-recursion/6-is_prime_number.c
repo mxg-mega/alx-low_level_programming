8f527365f7f2a16a92c1a175fc6772713e26e317
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-09-30 16:49:27 +0000

    prime nums

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * is_prime_number - a functions that determines a prime numbers
+  * @n: number
+  * Return: 1 if true
+*/
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else if (n == 2)
+	{
+		return (1;)
+	}
+	else if (n % 2 == 0)
+	{
+		return (0);
+	}
+	else
+	{
+	return (is_prime_number_recursive(n / 2 + 1));
+	}
+}
