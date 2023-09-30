29e00a21753d0e443114c637b4d33ac9ef629111
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-09-30 10:46:27 +0000

    sqrt recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * _sqrt_recursion - returns the square of an integer
+  * @n: integer
+  * Return: the square of integer
+*/
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+        	return (-1);
+    	}
+
+	if (n == 0 || n == 1)
+	{
+        	return (n);
+    	}
+
+	int start = 0;
+	int end = n;
+
+	while (start <= end)
+	{
+        	int mid = start + (end - start) / 2;
+        	int sqr = mid * mid;
+
+        	if (sqr == n)
+		{
+            		return (mid);
+        	}
+
+	        if (sqr < n)
+		{
+        	    start = mid + 1;
+	        }
+		else
+		{
+	            end = mid - 1;
+        	}
+    	}
+
+    return (-1);
+}
