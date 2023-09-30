0b59da86d0207511b7d884fb6304f2b288d9a19e
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-09-30 02:57:33 -0700

    fix strlen recursion 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,14 +6,15 @@
   * @s: string
   * Return: lenght of string
 */
-int _strlen_recursion(char *s) {
+int _strlen_recursion(char *s)
+{
 	if (*s == '\0')
 	{
-		return 0;
+		return (0);
 	}
 	else
 	{
-		return 1 + _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
 }
 
