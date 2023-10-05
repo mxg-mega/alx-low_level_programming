d06b28b2d9abc61c3ef491016d18de4a178bdb2e
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-10-05 15:52:40 +0000

    string concat with space in between

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,10 @@ char *str_concat(char *s1, char *s2)
 	char *new_str;
 	int len1, len2, tlen, i, j;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	len1 = 0;
 	while (s1[len1] != '\0')
 	{
