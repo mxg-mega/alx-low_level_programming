bfe48d3db5df60fb05cd6a41b4b42952c03d5b34
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-10-05 19:43:21 +0000

    alloc grid fix 14

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,7 +27,7 @@ int **alloc_grid(int width, int height)
 		h_array[i] = (int *)malloc(sizeof(int) * width);
 		if (h_array[i] == NULL)
 		{
-			for (j = 0; j < width; j++)
+			for (; i >= 0; i--)
 			{
 				free(h_array[i]);
 			}
