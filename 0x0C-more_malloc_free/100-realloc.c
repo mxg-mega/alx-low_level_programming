3427c36d91b4c1c83bc7292b4c6c5639e8d76933
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-10-10 21:49:24 +0000

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,5 +13,36 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_array;
+	unsigned char *old, *new;
+	unsigned int i;
+
+	if (ptr == NULL)
+	{
+		return (malloc(new_size));
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+
+	new_array = malloc(new_size);
+	if (new_array == NULL)
+	{
+		return (NULL);
+	}
+
+	old = ptr;
+	new = new_array;
+	for (i = 0; i < old_size && i < new_size; i++)
+	{
+		new[i] = old[i];
+	}
+
+	free(ptr);
 	return (new_array);
 }
