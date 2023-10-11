0b71b890a0a4fa776cd7163234ead8070d475685
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-10-11 17:11:35 +0000

    new dog struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+  * new_dog - functions creates a new instance of struct dog
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: name of new owner
+  * Return: a pointer to the new instance
+  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+	char *n, *o;
+
+	if (name == NULL)
+	{
+		name = "";
+	}
+	if (owner == NULL)
+	{
+		owner = "";
+	}
+	n = name;
+	o = owner;
+	d = malloc(sizeof(d));
+	if (d == NULL)
+	{
+		return (NULL);
+	}
+	d->name = n;
+	d->age = age;
+	d->owner = o;
+	return (d);
+}
