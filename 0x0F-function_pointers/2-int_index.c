387d7683823209215d393ae164ba264a0076096f
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-10-14 20:04:38 +0000

    search int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,3 +2,29 @@
 #include "function_pointers.h"
 
 /**
+  * int_index - function search for an integer
+  * @array: the array
+  * @size: size of array
+  * @cmp: function to execute
+  *
+  * Return: the searched integer
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
