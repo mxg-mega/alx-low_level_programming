e15c9c739404a1273395049dc3cd8297bf8d76d3
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-10-16 09:31:58 +0000

    opcodes main

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * printOpcodes - Print the opcodes of the main function
+ * @numBytes: The number of bytes to print
+ */
+void printOpcodes(int numBytes)
+{
+	unsigned char *mainAddr = (unsigned char *)printOpcodes;
+
+	for (int i = 0; i < numBytes; i++)
+	{
+		if (i > 0)
+		{
+			printf(" "); // Separate opcodes with a space
+		}
+		printf("%02x", mainAddr[i]);
+	}
+	printf("\n");
+}
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array of command-line arguments
+ * Return: 0 on success, 1 on incorrect argument count, 2 on negative input
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int numBytes = atoi(argv[1]);
+
+	if (numBytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	printOpcodes(numBytes);
+
+	return (0);
+}
