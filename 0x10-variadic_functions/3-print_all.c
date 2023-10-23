7f5cc0e8ece927b35c00a122f3ab6e537d6515c6
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-10-23 05:47:45 -0700

    check code

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,59 +2,91 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+
+typedef struct specifier
+{
+	char spe;
+	void (*f)(va_list args);
+} specifier_t;
+
 /**
- * print_all - Print various data types with a separator.
+ * print_char - Print a char.
+ * @args: Argument list.
+ */
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - Print an int.
+ * @args: Argument list.
+ */
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+/**
+ * print_float - Print a float.
+ * @args: Argument list.
+ */
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+ * print_string - Print a string.
+ * @args: Argument list.
+ */
+void print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	if (str == NULL)
+		printf("(nil)");
+	else
+		printf("%s", str);
+}
+
+/**
+ * print_all - Print various data types based on a format string.
  * @format: A format string indicating data types.
  * @...: Variable number of arguments corresponding to the format.
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	const char *format_copy = format;
-	char *separator = "";
-	int printed = 0;
+	int i, j;
+	specifier_t specs = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string}
+	};
 
+
+	i = 0;
 	va_start(args, format);
 
-    while (format_copy && *format_copy)
-    {
-        if (*format_copy == 'c')
-        {
-            printf("%s%c", separator, va_arg(args, int));
-            printed = 1;
-        }
-        else if (*format_copy == 'i')
-        {
-            printf("%s%d", separator, va_arg(args, int));
-            printed = 1;
-        }
-        else if (*format_copy == 'f')
-        {
-            printf("%s%f", separator, (float)va_arg(args, double));
-            printed = 1;
-        }
-        else if (*format_copy == 's')
-        {
-            char *str = va_arg(args, char *);
-            if (str == NULL)
-            {
-                printf("%s(nil)", separator);
-            }
-            else
-            {
-                printf("%s%s", separator, str);
-            }
-            printed = 1;
-        }
+	while (format && format[i])
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == specs[j].spe)
+			{
+				specs[j].f(args);
+				if (format[i + 1])
+					printf(", ");
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
 
-        if (printed)
-        {
-            separator = ", ";
-            printed = 0;
-        }
-
-        format_copy++;
-    }
 	printf("\n");
 	va_end(args);
 }
+
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,23 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
+#include <stdarg.h>
 
+/**
+ * struct specifier - A struct for format specifiers.
+ * @c: Function pointer to print a char.
+ * @i: Function pointer to print an int.
+ * @f: Function pointer to print a float.
+ * @s: Function pointer to print a string.
+ */
+/*
+typedef struct specifier
+{
+	void (*c)(va_list args);
+	void (*i)(va_list args);
+	void (*f)(va_list args);
+	void (*s)(va_list args);
+} specifier_t;
+*/
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
