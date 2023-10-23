ba19e1271b9db0dcf64982b10990a70c03f779d7
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-10-23 06:36:31 -0700

    removed the struct specifier from headerfile

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,17 +2,6 @@
 #define _VARIADIC_FUNCTIONS_H_
 #include <stdarg.h>
 
-/**
- * struct specifier - A struct for format specifiers.
- * @spe: specifier character.
- * @f: Function pointer to print a float.
- */
-typedef struct specifier
-{
-	char spe;
-	void (*f)(va_list args);
-} specifier_t;
-
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
