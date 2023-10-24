f5f31ad755285fdbfb586022d7d24e7162ed2a86
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-10-24 09:28:17 -0700

    checck printlist

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,4 +7,27 @@
  * @h: head struct of the linked list
  * Return: a size_t
  */
-size_t print_list(const list_t *h){}
+size_t print_list(const list_t *h)
+{
+	list_t *currentNode;
+	size_t n;
+
+	n = 0;
+	currentNode = h;
+	while (currentNode->next != NULL)
+	{
+		printf("[%d] ", currentNode->len);
+		if (currentNode->str != NULL)
+		{
+			printf("%s", currentNode->str);
+		}
+		else
+		{
+			printf("(nil)");
+		}
+		n++;
+		currentNode = h->next;
+	}
+	return (n);
+}
+
