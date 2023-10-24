08da66b7bc8a6d9670e86f20119acee16a783edc
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-10-24 09:13:34 -0700

    added the struct declaratiotion

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_lists - print the values of a linked list
+ * @h: head struct of the linked list
+ * Return: a size_t
+ */
+size_t print_list(const list_t *h){}
