9e6a1f62c7712bfa9c311685b7bb02da9a6e2550
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-10-25 20:52:12 +0000

    100 first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  * Return: always 0
+  */
+int main(void)
+{
+	print_function();
+	return (0);
+}
+
+/**
+  * print_function - prints a statement
+  * Return: no return
+  */
+void print_function(void)
+{
+	printf("You're beat! And yet, you must allow,\n
+I bore my house upon my back!\n");
+}
+
