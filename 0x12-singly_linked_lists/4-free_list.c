a815c0913f1937280f4d4236abaa9fc0f8503aa9
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-10-25 20:10:53 +0000

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * free_list - deletes an entire list
+  * @head: head node of list
+  * Return: no return
+  */
+void free_list(list_t *head)
+{
+	list_t *current;
+	list_t *next;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	current = head;
+	while (current->next !=NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+	free(next->str);
+	free(next);
+}
