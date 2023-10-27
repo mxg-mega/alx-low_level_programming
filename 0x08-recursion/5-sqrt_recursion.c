b57ed54fdb2401fec6d259f2e9e3a54aba492eec
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-10-27 20:07:21 +0000

    fix three tasks

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,32 +1,37 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
-  * _sqrt_recursion - returns the square of an integer
-  * @n: integer
-  * Return: the square of integer
-*/
+ * sqr - returns the natural square root of a number
+ * @n: input number
+ * @y: number of iterator
+ * Return: square root or -1
+ */
+
+int sqr(int n, int y)
+{
+	if (y % (n / y) == 0)
+	{
+		if (y * (n / y) == n)
+			return (y);
+		else
+			return (-1);
+	}
+	return (0 + sqr(n, y + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: input number
+ * Return: -1 if n does not have a natural square root
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-	{
-    		return (-1);
-  	}
-	else if (n == 0 || n == 1)
-	{
-    		return (n);
-  	}
-	else
-	{
-    		int mid = (n / 2) + (n % 2);
-    		int sqrt = _sqrt_recursion(n - (mid * mid));
-    		if (sqrt == -1)
-		{
-      			return (-1);
-    		}
-		else
-		{
-      			return (mid + sqrt);
-    		}
-  	}
+		return (-1);
+	else if (n == 0)
+		return (0);
+	else if (n == 1)
+		return (1);
+	return (sqr(n, 2));
 }
+
