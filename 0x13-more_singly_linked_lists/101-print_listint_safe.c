9933c9338a91863e829b87c8e036233ff907f019
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-10-27 21:01:28 +0000

    print list int

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,23 +8,29 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t num = 0;
-	long int diff;
+	size_t count = 0;
 
-	while (head)
+	while (head != NULL)
 	{
-		diff = head - head->next;
-		num++;
 		printf("[%p] %d\n", (void *)head, head->n);
-		if (diff > 0)
-			head = head->next;
-		else
+		count++;
+		head = head->next;
+
+		/* Check for a loop by comparing the current address to previous nodes */
+		const listint_t *slow = head, *fast = head;
+
+		while (fast != NULL && fast->next != NULL)
 		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			break;
+			slow = slow->next;
+			fast = fast->next->next;
+			if (slow == fast)
+			{
+				printf("-> [%p] %d\n", (void *)slow, slow->n);
+				exit(98);
+			}
 		}
 	}
 
-	return (num);
+	return count;
 }
 
