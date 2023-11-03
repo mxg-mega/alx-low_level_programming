992e9e524b05db3a8fd5d6b768feac75b3b216a4
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-11-03 16:55:59 +0000

    read a text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,20 +18,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-
 	fileopen = open(filename, O_RDWR);
 	if (fileopen == -1)
 	{
 		return (0);
 	}
-
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
 		close(fileopen);
 		return (0);
 	}
-
 	file_read = read(fileopen, buffer, letters);
 	if (file_read == -1)
 	{
@@ -39,7 +36,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fileopen);
 		return (0);
 	}
-
 	count = write(STDOUT_FILENO, buffer, file_read);
 	if (count == -1 || count != file_read)
 	{
@@ -47,7 +43,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fileopen);
 		return (0);
 	}
-
 	close(fileopen);
 	free(buffer);
 	return (file_read);
