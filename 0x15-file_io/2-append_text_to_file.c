d2b25228baa29cff80fdf0f45b638d14c764f56d
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-11-03 18:12:11 +0000

    append a text to file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  * append_text_to_file - adds a text to a txt file
+  * @filename: name of the file
+  * @text_content: text to append
+  *
+  * Return:
+  */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int openfile;
+	ssize_t writefile;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	openfile = open(filename, O_RDWR | O_APPEND);
+	if (openfile == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		writefile = write(openfile, text_content, strlen(text_content));
+		if (writefile == -1)
+		{
+			close(openfile);
+			return (-1);
+		}
+	}
+	close(openfile);
+	return (1);
+}
