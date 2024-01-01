c62b7bc78a37b2cc47299a577968faf845aa885e
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-01-01 14:07:14 +0000

    counting nodes without printing them

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+  * dlistin_len - counts the nodes on a doubly linked list
+  * @h: the head of the list
+  *
+  * Return: the number of nodes
+  */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int length = 0;
+	const dlistint_t *current = NULL;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	current = h;
+	while (current != NULL)
+	{
+		current = current->next;
+		length++;
+	}
+	return (length);
+}
