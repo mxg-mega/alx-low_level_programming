1be3f318f195681dc18883d97b77b9283b0bab1c
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-01-02 23:07:03 +0000

    fix for insert a node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,7 +35,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (i < idx)
 	{
-		return (NULL);
+		return (add_nodeint(h, n));
 	}
 
 	newNode->next = current;
