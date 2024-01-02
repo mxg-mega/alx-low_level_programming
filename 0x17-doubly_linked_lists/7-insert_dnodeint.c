69738e2953d07eb8381d648996febc0fd1ca109c
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-01-02 23:20:19 +0000

    fix for insert a node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -37,7 +37,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-	if (i == idx)
+	if (i+1 == idx)
 	{
 		return (add_dnodeint_end(h, n));
 	}
