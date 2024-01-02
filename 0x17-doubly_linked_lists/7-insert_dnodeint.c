cd5ce317d12c8701e8d4d5d5b51335e21f17f739
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-01-02 22:19:49 +0000

    fix for insert a node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (*h == NULL)
 	{
-		return (add_dnodeint(h, n));
+		return (NULL);
 	}
 	newNode = malloc(sizeof(dlistint_t));
 	if (newNode == NULL)
