46a8d3baf3be7f1577d1c5392e1641ec5dc210b7
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-01-04 14:44:08 +0000

    fixed delet node at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -22,6 +22,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		tracker++;
 		current = current->next;
 	}
+	if (tracker < index)
+	{
+		return (-1);
+	}
 	if (current->prev != NULL)
 	{
 		current->prev->next = current->next;
