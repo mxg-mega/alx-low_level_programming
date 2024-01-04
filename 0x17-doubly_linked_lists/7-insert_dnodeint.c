53a13f181a8293ff8c8181aca592744a4190c606
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-01-04 13:40:23 +0000

    use the checker

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *current = NULL, *newNode = NULL;
 	unsigned int i = 0;
 
-	if (*h == NULL)
+	if (*h == NULL || idx > dlistint_len(*h))
 	{
 		return (add_dnodeint(h, n));
 	}
