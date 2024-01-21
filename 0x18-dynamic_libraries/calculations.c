8dc13a525368b43895a56049b5092977b6648b47
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-01-21 03:49:41 -0800

    now i canuse C defined functions in python i can't be lonely anymorepython3 100-tests.py

---
+++ 0x18-dynamic_libraries/calculations.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int add(int num1,int num2)
+{
+	return (num1 + num2);
+}
+int sub(int num1,int num2)
+{
+	return (num1 - num2);
+}
+int mul(int num1,int num2)
+{
+	return (num1 * num2);
+}
+int div(int num1,int num2)
+{
+	return (num1 / num2);
+}
+int mod(int num1,int num2)
+{
+	return (num1 % num2);
+}
