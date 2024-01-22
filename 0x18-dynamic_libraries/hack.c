751d5fe17c5ad497670cd049794eccb2e2657b4c
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-01-22 12:46:53 -0800

    checker 6

--- 0x18-dynamic_libraries/hack.c
+++ 0x18-dynamic_libraries/hack.c
@@ -3,7 +3,7 @@
 
 int rand(void)
 {
-    static int numbers[] = {9, 8, 10, 24, 75, 9};
+    static int numbers[6] = {9, 8, 10, 24, 75, 9};
     static int index = 0;
     
     return (numbers[index++]);
