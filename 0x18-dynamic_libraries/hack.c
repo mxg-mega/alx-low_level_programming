7e8e5cd84d50339428a09d6ecf9e5c5067f028db
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-01-22 12:45:13 -0800

    checker 5

--- 0x18-dynamic_libraries/hack.c
+++ 0x18-dynamic_libraries/hack.c
@@ -1,18 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-// Function to override srand
-void srand(unsigned int seed) {
-    // Set a specific seed for consistent results
-    srand(73);
-}
-
-int rand(void) {
-    // Return the expected winning numbers
-    // You might need to adjust these values based on the expected format
+int rand(void)
+{
     static int numbers[] = {9, 8, 10, 24, 75, 9};
     static int index = 0;
     
-    return numbers[index++];
+    return (numbers[index++]);
 }
 
