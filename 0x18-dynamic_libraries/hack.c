f3fd85d22931ab10ef76019a36a2214ae3797e6e
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-01-22 11:54:56 -0800

    gigga machine trial 3

--- 0x18-dynamic_libraries/hack.c
+++ 0x18-dynamic_libraries/hack.c
@@ -1,12 +1,5 @@
 #include <stdlib.h>
 
-void srand(unsigned int seed) {
-    // Override srand to ensure consistent random number generation
-    // Use a specific seed value to generate predictable numbers
-    // You might need to experiment with the seed value for better results
-    srand(123);
-}
-
 int rand(void) {
     // Return the expected winning numbers
     // You might need to adjust these values based on the expected format
