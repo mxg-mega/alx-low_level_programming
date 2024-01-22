f8a18140cd7504ef81e5238418fe4fcf90f56afd
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-01-22 11:32:48 -0800

    gigga machine trial 1

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p /tmp/ https://github.com/mxg-mega/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmakewin.so
+wget -P /tmp/ https://github.com/mxg-mega/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmakewin.so
 export LD_PRELOAD=/tmp/libmakewin.so
