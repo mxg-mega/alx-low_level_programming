71998607e443f7d4ea736062ef7b7e7747769b4f
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-02-19 16:46:38 +0000

    creating a hash table via a function

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function creates a hash table.
+ * @size: size of the hash table array
+ *
+ * Return: a pointer to the newly created hash table
+ * or NULL if fail
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_hash_table = NULL;
+	
+	new_hash_table = malloc(sizeof(hash_table_t));
+	if (new_hash_table == NULL)
+	{
+		return (NULL);
+	}
+	new_hash_table->size = size;
+	if (size <= 0)
+	{
+		free(new_hash_table);
+		return (NULL);
+	}
+	new_hash_table->array = malloc(sizeof(hash_node_t*) * size);
+	if (new_hash_table == NULL)
+	{
+		free(new_hash_table);
+		return (NULL);
+	}
+	return (new_hash_table);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,1 @@
+hash table is a very useful data structure that help retrieve information in a relatively fast way
