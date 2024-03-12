4f72907a56c2ae325bb488d11c32d984636226db
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-03-12 12:59:19 +0000

    key index conversion

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - function get the index of a key
+ * @key: the string
+ * @size: the size of the array
+ *
+ * Return: returns the index of the array
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	index = hash_djb2(key) % size;
+	return (index);
+}
