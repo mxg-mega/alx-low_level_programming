6bc34ce3fea64a398d303f2c3e21911200f637bd
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2024-10-23 16:53:05 +0100

    island perimeter 6

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -24,14 +24,19 @@ def island_perimeter(grid):
                     if (j + 1) < len(grid[i]):
                         if grid[i][j + 1] == 1:
                             perimeter -= 1
+
                 if j <= len(grid[i]) and j > 0:
-                    if (j - 1) > 0:
+                    if (j - 1) >= 0:
                         if grid[i][j - 1] == 1:
                             perimeter -= 1
 
-                if i >= 0 and i < len(grid) and (i + 1) < len(grid):
-                    if grid[i + 1][j] == 1:
-                        perimeter -= 1
+
+                if i >= 0 and i < len(grid):
+                    if (i + 1) < len(grid):
+
+                        if grid[i + 1][j] == 1:
+                            perimeter -= 1
+
                 if i <= len(grid) and i > 0 and (i - 1) >= 0:
                     if grid[i - 1][j] == 1:
                         perimeter -= 1
